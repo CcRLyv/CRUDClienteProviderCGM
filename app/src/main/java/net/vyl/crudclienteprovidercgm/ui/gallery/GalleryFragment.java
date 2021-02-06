361a3d7d3a6e10855dcb0d1b35088dbfc3ac9d79
Giovanni Medina <grablens_war@hotmail.com>
2021-02-06 14:56:20 -0600

    Implementacion del insert

--- app/src/main/java/net/vyl/crudclienteprovidercgm/ui/gallery/GalleryFragment.java
+++ app/src/main/java/net/vyl/crudclienteprovidercgm/ui/gallery/GalleryFragment.java
@@ -1,10 +1,14 @@
 package net.vyl.crudclienteprovidercgm.ui.gallery;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,22 +18,42 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import net.vyl.crudclienteprovidercgm.R;
+import net.vyl.crudclienteprovidercgm.provider.MiProveedorContenidoContract;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    Button btnAceptar;
+    TextView tbNombre;
+    TextView tbCorreo;
+    TextView tbContra;
+    TextView tbTel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+
+        btnAceptar = root.findViewById(R.id.btnAceptar);
+        tbNombre  = root.findViewById(R.id.tbNombre);
+        tbCorreo  = root.findViewById(R.id.tbEmail);
+        tbContra  = root.findViewById(R.id.tbPas);
+        tbTel  = root.findViewById(R.id.tbTel);
+
+
+
+        ContentResolver cr = getContext().getContentResolver();
+        ContentValues cv = new ContentValues();
+
+        btnAceptar.setOnClickListener(l->{
+            cv.put(MiProveedorContenidoContract.Usuarios.NOMBRE,tbNombre.getText().toString() );
+            cv.put(MiProveedorContenidoContract.Usuarios.PASS, tbContra.getText().toString());
+            cv.put(MiProveedorContenidoContract.Usuarios.EMAIL, tbCorreo.getText().toString());
+            cv.put(MiProveedorContenidoContract.Usuarios.TELEFONO, tbContra.getText().toString());
+
+            Uri resultado = cr.insert(MiProveedorContenidoContract.Usuarios.CONTENT_URI, cv);
+
         });
         return root;
     }
